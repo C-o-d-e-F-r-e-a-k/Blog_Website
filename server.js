7c1aba69aaea5090fbd64a6b8ccc22db5fa47b26
C-o-d-e-F-r-e-a-k <bumblebee1742004@gmail.com>
2024-07-16 13:58:17 +0530

    secind commit

--- server.js
+++ server.js
@@ -1,20 +1,56 @@
-const express = require('express')
-const articleRouter = require("./routes/articles")
-const Article = require('./models/article')
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-const app = express()
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const articleRouter = require("./routes/articles");
+const Article = require("./models/article");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
-mongoose.connect('mongodb://localhost/bharatInternDatabase')
-app.set("views", "./view")
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
-app.use(methodOverride('_method'))
-app.get('/', async(req, res) => {
-    const articles =await Article.find().sort({ createdAt:'desc'})
-    res.render('articles/index', { articles: articles })
-})
+const app = express();
+dotenv.config();
 
-app.use('/articles', articleRouter)
+// const port = process.env.PORT || 5000;
 
-app.listen(3000)
+const username = process.env.mongodb_username;
+const password = process.env.mongodb_password;
+
+if (!username || !password) {
+  console.error(
+    "MongoDB username or password not set in environment variables"
+  );
+  process.exit(1);
+}
+
+mongoose
+  .connect(
+    `mongodb+srv://${username}:${password}@cluster-1.cxgc1yb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1`
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+    process.exit(1);
+  });
+
+app.set("views", "./view");
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the "public" directory.
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  try {
+    const articles = await Article.find().sort({ createdAt: "desc" });
+    res.render("articles/index", { articles: articles });
+  } catch (err) {
+    res.status(500).send("Error fetching articles");
+  }
+});
+
+app.use("/articles", articleRouter);
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
